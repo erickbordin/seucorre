8651d53d6480d371d42df5639dbf5c818ff1d005
Érick Souza Bordin <ericksouzabordin3@gmail.com>
2026-04-28 17:40:30 -0300

    Refactor UsuarioRepository by removing method
    Removed the findByEmail method and added Repository annotation.

--- src/main/java/com/seucorre/usuario/infrastructure/UsuarioRepository.java
+++ src/main/java/com/seucorre/usuario/infrastructure/UsuarioRepository.java
@@ -2,10 +2,8 @@ package com.seucorre.usuario.infrastructure;
 
 import com.seucorre.usuario.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID; // Importar aqui também
-import java.util.Optional;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
 
-// Mudamos o segundo parâmetro de String para UUID
-public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
-    Optional<Usuario> findByEmail(String email);
-}
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {}
